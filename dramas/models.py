be28d0ae6b0fda29197a8bf2372339d8dbeffc2a
YASICJUNWOO <joonoo3@naver.com>
2023-01-09 02:15:37 +0900

    add genre to drama

--- dramas/models.py
+++ dramas/models.py
@@ -5,3 +5,7 @@ class Drama(models.Model):
 
     title = models.CharField(max_length=150)
     description = models.TextField(default="")
+    genre = models.ManyToManyField(
+        "genre.Genre",
+        blank=True,
+    )
